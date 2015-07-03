315274f3b3ecaa5d1b688259db242eb389954cb3
Mark Jenkins <mark@parit.ca>
2015-07-03 15:42:06 -0500

    refactor with key aspects in re-useable functions

--- xmpp_thesign.py
+++ xmpp_thesign.py
@@ -7,7 +7,7 @@ import time
 SERVER = 'skullspace.ca'
 SIGN_POST_USER = 'thesign@skullspace.ca'
 
-if __name__ == "__main__":
+def create_connected_authed_client():
     with file('userpass') as f:
         username = f.readline().strip()
         passwd = f.readline().strip()
@@ -15,18 +15,22 @@ if __name__ == "__main__":
     client = xmpp.Client(SERVER)
     client.connect()
     client.auth(username, passwd)
+    return client
 
-    for line in sys.stdin:
+def send_line_iterable_w_send_delay(client, line_iterable, delay=1):
+    for line in line_iterable:
         client.send(xmpp.Message(
                 SIGN_POST_USER,
                 line.strip() + "\n" ))
-        time.sleep(1)
+        time.sleep(delay)
+
+if __name__ == "__main__":
+    client = create_connected_authed_client()
+
+    send_line_iterable_w_send_delay(client, sys.stdin, 1)
 
     for file_name in sys.argv[1:]:
         with open(file_name) as f:
-            for i, line in enumerate(f):
-                client.send(xmpp.Message(
-                        SIGN_POST_USER,
-                        line.strip() + "\n" ) )
-                time.sleep(1)
+            send_line_iterable_w_send_delay(client, f, 1)
+
 
